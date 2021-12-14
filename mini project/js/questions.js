59c579a5a52ff6c167cfcc50ebf42dddb7bfd74c
rohithreddy2001 <95706806+rohithreddy2001@users.noreply.github.com>
2021-12-14 17:42:12 +0530

    updated

---
+++ mini project/js/questions.js
@@ -0,0 +1,127 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "How many computer languages are in use?",
+    answer: "2000",
+    options: [
+      "20",
+      "50",
+      "5000",
+      "2000"
+    ]
+  },
+    {
+    numb: 2,
+    question: "Who founded Apple Computer?",
+    answer: "Steve Jobs",
+    options: [
+      "Stephen Hawking",
+      "Steve Jobs",
+      "Bill Gates",
+      "Stephen Fry"
+    ]
+  },
+    {
+    numb: 3,
+    question: "Which of these is not a peripheral, in computer terms?",
+    answer: "Motherboard",
+    options: [
+      "Monitor",
+      "Keyboard",
+      "Motherboard",
+      "Mouse"
+    ]
+  },
+    {
+    numb: 4,
+    question: "Which of the following is not one of the early “protocols,” or ways to use the Internet?",
+    answer: "Blogging",
+    options: [
+      "Telnet",
+      "Gopher",
+      "FTP",
+      "Blogging"
+    ]
+  },
+    {
+    numb: 5,
+    question: "What does the Internet prefix WWW stand for?",
+    answer: "World Wide Web",
+    options: [
+      " Western Washington World",
+      "Worldwide Weather",
+      "Wide Width Wickets",
+      "World Wide Web"
+    ]
+  },
+  {
+    numb: 6,
+    question: "Which of these products is not made by the Apple Corporation?",
+    answer: "IMAX",
+    options: [
+      "iPod",
+      "iMac",
+      "iPhone",
+      "IMAX"
+    ]
+  },
+  {
+    numb: 7,
+    question: "What is the name for a computer pointing device?",
+    answer: "A Mouse",
+    options: [
+      "Ram",
+      "Monitor",
+      "Sound Card",
+      "A Mouse"
+    ]
+  },
+  {
+    numb: 8,
+    question: "When did the compact disc first appear on the market?",
+    answer: "1982",
+    options: [
+      "1989",
+      "1955",
+      "1992",
+      "1982"
+    ]
+  },
+  {
+    numb: 9,
+    question: "To which of these devices is the cellular telephone most closely related?",
+    answer: "Radio",
+    options: [
+      "Telescope",
+      "Light bulb",
+      "Telegraph",
+      "Radio"
+    ]
+  },
+  {
+    numb: 10,
+    question: " What does fiber optic cable resemble, in terms of size?",
+    answer: "human hair",
+    options: [
+      "Telephone wire",
+      "Bamboo",
+      "Pipeline",
+      "human hair"
+    ]
+  },
+  // you can uncomment the below codes and make duplicate as more as you want to add question
+  // but remember you need to give the numb value serialize like 1,2,3,5,6,7,8,9.....
+
+  //   {
+  //   numb: 6,
+  //   question: "Your Question is Here",
+  //   answer: "Correct answer of the question is here",
+  //   options: [
+  //     "Option 1",
+  //     "option 2",
+  //     "option 3",
+  //     "option 4"
+  //   ]
+  // },
+];
